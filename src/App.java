000ae56787dd5e4279991a0b7669761b7446f438
Iusti T <iustilaurentiu@yahoo.ro>
2022-08-25 19:33:06 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+public class App {
+    public static void print(String word){
+        System.out.println(word);
+    }
+    public static void main(String[] args) throws Exception {
+        
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D Adventure");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible((true));
+        }
+    }
+}
